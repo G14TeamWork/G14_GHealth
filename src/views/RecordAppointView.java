e3988c7d5f3f37385f538ba8436dd51848887b97
Ruslan <Ruslan@10.100.102.3>
2016-06-04 17:55:42 +0300

    try

--- src/views/RecordAppointView.java
+++ src/views/RecordAppointView.java
@@ -1,14 +1,22 @@
 package views;
 
 import graphics.GUIimage;
+
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JSeparator;
+
 import mainPackage.MainClass;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+
+import javax.swing.JTextPane;
+
+import java.awt.Color;
 
 public class RecordAppointView extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -23,18 +31,34 @@ public class RecordAppointView extends JPanel {
 		
 		JLabel lblExpert = new JLabel("Record Appointment");
 		lblExpert.setFont(new Font("Lucida Grande", Font.BOLD, 22));
-		lblExpert.setBounds(211, 182, 253, 118);
+		lblExpert.setBounds(192, 136, 253, 118);
 		add(lblExpert);
 		
-		JButton btnBack = new JButton("Back");
-		btnBack.addActionListener(new ActionListener() {
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setForeground(Color.BLACK);
+		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				MainClass.masterControler.setView(
-						MainClass.masterControler.EXPVCont.expview);
+				if(JOptionPane.showConfirmDialog(null, "Cancel record?",null,JOptionPane.YES_NO_OPTION)==0)
+					MainClass.masterControler.setView(MainClass.masterControler.EXPVCont.expview);
 			}
 		});
-		btnBack.setBounds(490, 440, 140, 55);
-		add(btnBack);
-		btnBack.setIcon(new GUIimage("back", 25, 23).image);
+		btnCancel.setBounds(493, 462, 140, 55);
+		add(btnCancel);
+		btnCancel.setIcon(null);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(30, 212, 421, 221);
+		add(textPane);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(JOptionPane.showConfirmDialog(null, "Save Record?",null,JOptionPane.YES_NO_OPTION)==0)
+					MainClass.masterControler.setView(MainClass.masterControler.EXPVCont.expview);
+			}
+		});
+		btnSave.setBounds(493, 382, 140, 55);
+		add(btnSave);
+		
 	}
 }

292eed626bdf95d2f2628c9d65d35130bd043b13
yshavat89 <yshavat89@gmail.com>
2016-06-04 23:08:12 +0300

    cancel appointment controller cancel appointment view

--- src/Controllers/CancelAppointmentController.java
+++ src/Controllers/CancelAppointmentController.java
@@ -1,8 +1,12 @@
 package Controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
+import ocsf.server.GHealthServer;
 import views.CancelAppointmentView;
 import Controllers.IRefresh;
 
@@ -11,7 +15,22 @@ public class CancelAppointmentController implements Observer,IRefresh  {
 	
 	public CancelAppointmentController() {
 		 CancelAppointmentview = new  CancelAppointmentView();
+
 	}
+	
+	public void toSQL(String idpatient )
+	{
+				ArrayList<Object> arrList = new ArrayList<Object>();
+				String query="	SELECT  ghealth.appointments.appdate,ghealth.expert.experties,ghealth.users.firstname,ghealth.users.lastname"
+								+"FROM ghealth.appointments,ghealth.users,ghealth.expert" 
+								+"WHERE ghealth.appointments.idpatient="+ idpatient+"AND"
+								+"ghealth.appointments.idexpert = ghealth.expert.id AND"
+								+"ghealth.users.username = ghealth.expert.id";
+				arrList = GHealthServer.sqlConn.sendSqlQuery(query);
+				for (int i  = 0 ; i < arrList.size() ; i +=4)
+				CancelAppointmentview.getComboBox().addItem((String)arrList.get(i)+" "+ (String)arrList.get(i+1)+" "+(String)arrList.get(i+2)+" "+(String)arrList.get(i+3));
+	}
+		
 	@Override
 	public void refreshView() {
 		// TODO Auto-generated method stub
--- src/views/CancelAppointmentView.java
+++ src/views/CancelAppointmentView.java
@@ -1,18 +1,35 @@
 package views;
 
 import graphics.GUIimage;
+
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JSeparator;
+
 import mainPackage.MainClass;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 
+import javax.swing.JTextField;
+import javax.swing.JLayeredPane;
+import javax.swing.JDesktopPane;
+import javax.swing.JScrollBar;
+import javax.swing.JSpinner;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+
+import ocsf.server.GHealthServer;
+
 public class CancelAppointmentView extends JPanel {
 	private static final long serialVersionUID = 1L;
 	public JSeparator separator;
+	private JTextField searchField;
+	private JButton btnSerch;
+	private JComboBox comboBox;
+	
 	public CancelAppointmentView() {
 		setLayout(null);
 		this.setBounds(0, 0, 677, 562);
@@ -36,5 +53,50 @@ public class CancelAppointmentView extends JPanel {
 		btnBack.setBounds(490, 440, 140, 55);
 		add(btnBack);
 		btnBack.setIcon(new GUIimage("back", 25, 23).image);
+		
+		searchField = new JTextField();
+		searchField.setToolTipText("");
+		searchField.setText("type patient id ");
+		searchField.setBounds(73, 280, 401, 20);
+		add(searchField);
+		searchField.setColumns(10);
+		
+		JButton btnSerch = new JButton("");
+		btnSerch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				MainClass.masterControler.CACont.toSQL(searchField.getText());
+			}
+			});
+
+		btnSerch.setBounds(492, 279, 31, 21);
+		add(btnSerch);
+		
+		comboBox = new JComboBox();
+		comboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		comboBox.setBounds(73, 311, 401, 20);
+		add(comboBox);
+	}
+	public JComboBox getComboBox() {
+		return comboBox;
+	}
+	public void setComboBox(JComboBox comboBox) {
+		this.comboBox = comboBox;
+	}
+	public JTextField getSearchField() {
+		return searchField;
+	}
+	public void setSearchField(JTextField searchField) {
+		this.searchField = searchField;
+	}
+	public JButton getBtnSerch() {
+		return btnSerch;
+	}
+	public void setBtnSerch(JButton btnSerch) {
+		this.btnSerch = btnSerch;
 	}
 }

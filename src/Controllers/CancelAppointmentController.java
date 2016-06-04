accdc54599e6af7c883c636e6540b22603802b2f
gal51m1 <gal51m1@gmail.com>
2016-06-04 14:23:04 +0300

    save

---
+++ src/Controllers/CancelAppointmentController.java
@@ -0,0 +1,25 @@
+package Controllers;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import views.CancelAppointmentView;
+import Controllers.IRefresh;
+
+public class CancelAppointmentController implements Observer,IRefresh  {
+	public  CancelAppointmentView  CancelAppointmentview;
+	
+	public CancelAppointmentController() {
+		 CancelAppointmentview = new  CancelAppointmentView();
+	}
+	@Override
+	public void refreshView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+	}
+}

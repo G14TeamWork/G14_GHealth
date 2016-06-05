e68a5f6c0105c5de185ded31369dff68e6fb0f9e
eladfish <fish007fish@gmail.com>
2016-06-05 19:43:34 +0300

    set appointment upgrade fish

--- src/Controllers/SetAppointmentController.java
+++ src/Controllers/SetAppointmentController.java
@@ -30,7 +30,7 @@ public class SetAppointmentController implements Observer,IRefresh,Serializable 
 	public void checkExistanceSql(Patient pat)
 	{
 		String query = "";
-		query = "SELECT firstname,lastname FROM ghealth.patient where "
+		query = "SELECT firstname,lastname,phone,email,address FROM ghealth.patient where "
 				+ "id = \"" + pat.getId() + "\"";
 		arrList = GHealthServer.sqlConn.sendSqlQuery(query);
 		if (arrList.isEmpty())
@@ -41,6 +41,9 @@ public class SetAppointmentController implements Observer,IRefresh,Serializable 
 		{
 			pat.setFirstname((String)arrList.get(0));
 			pat.setLastname((String)arrList.get(1));
+			pat.setPhone((String)arrList.get(2));
+			pat.setEmail((String)arrList.get(3));
+			pat.setAddress((String)arrList.get(4));
 			arrList.clear();
 		}
 
@@ -58,8 +61,11 @@ public class SetAppointmentController implements Observer,IRefresh,Serializable 
 		{
 			pat.setFirstname(((Patient) arg).getFirstname());
 			pat.setLastname(((Patient) arg).getLastname());
-			SetAppointmentview.textField_first.setText(pat.getFirstname());
-			SetAppointmentview.textField_last.setText(pat.getLastname());
+			SetAppointmentview.textField_first.setText(((Patient) arg).getFirstname());
+			SetAppointmentview.textField_last.setText(((Patient) arg).getLastname());
+			SetAppointmentview.textField_phone.setText(((Patient) arg).getPhone());
+			SetAppointmentview.textField_email.setText(((Patient) arg).getEmail());
+			SetAppointmentview.textField_adress.setText(((Patient) arg).getAddress());
 			refreshView();
 		}
 	}
--- src/views/SetAppointmentView.java
+++ src/views/SetAppointmentView.java
@@ -27,11 +27,11 @@ public class SetAppointmentView extends JPanel {
 	private static final long serialVersionUID = 1L;
 	public JSeparator separator;
 	public JTextField textFieldid;
-	public String patientID;
 	public JTextField textField_first;
 	public JTextField textField_last;
-	public Patient pat;
-	ArrayList<Object> arrList = new ArrayList<>();
+	public JTextField textField_phone;
+	public JTextField textField_email;
+	public JTextField textField_adress;
 	public SetAppointmentView() {
 		setLayout(null);
 		this.setBounds(0, 0, 677, 562);
@@ -42,7 +42,7 @@ public class SetAppointmentView extends JPanel {
 		
 		JLabel lblExpert = new JLabel("Set Appointment");
 		lblExpert.setFont(new Font("Lucida Grande", Font.BOLD, 22));
-		lblExpert.setBounds(211, 182, 253, 55);
+		lblExpert.setBounds(255, 126, 253, 55);
 		add(lblExpert);
 		
 		JButton btnBack = new JButton("Back");
@@ -57,50 +57,80 @@ public class SetAppointmentView extends JPanel {
 		btnBack.setIcon(new GUIimage("back", 25, 23).image);
 		
 		textFieldid = new JTextField();
-		//textFieldid.setText("");
 		textFieldid.setColumns(10);
-		textFieldid.setBounds(271, 236, 148, 28);
+		textFieldid.setBounds(154, 171, 148, 28);
 		add(textFieldid);
 		
 		JLabel lblEnterPatientId = new JLabel("Enter patient ID:");
 		lblEnterPatientId.setFont(new Font("Dialog", Font.PLAIN, 16));
-		lblEnterPatientId.setBounds(146, 235, 120, 28);
+		lblEnterPatientId.setBounds(22, 170, 120, 28);
 		add(lblEnterPatientId);
 		
 		
-		JLabel searchIcon = new JLabel("");
+		JButton searchIcon = new JButton("");
 		searchIcon.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				MainClass.masterControler.SACont.setPatient();
 			}
 		});
-		searchIcon.setBounds(431, 236, 29, 28);
-		searchIcon.setIcon(new GUIimage("search",searchIcon.getWidth(),searchIcon.getHeight()).image);
+		searchIcon.setBounds(314, 171, 29, 28);
+		searchIcon.setIcon(new GUIimage("search",searchIcon.getWidth()-7,searchIcon.getHeight()-7).image);
 		this.add(searchIcon);
 		
 		textField_first = new JTextField();
 		textField_first.setEditable(false);
-		//textField_first.setText("");
 		textField_first.setColumns(10);
-		textField_first.setBounds(271, 277, 148, 28);
+		textField_first.setBounds(154, 209, 148, 28);
 		add(textField_first);
 		
 		textField_last = new JTextField();
 		textField_last.setEditable(false);
-		//textField_last.setText("");
 		textField_last.setColumns(10);
-		textField_last.setBounds(271, 318, 148, 28);
+		textField_last.setBounds(466, 209, 148, 28);
 		add(textField_last);
 		
 		JLabel lblFirstName = new JLabel("First name:");
 		lblFirstName.setFont(new Font("Dialog", Font.PLAIN, 16));
-		lblFirstName.setBounds(146, 276, 120, 28);
+		lblFirstName.setBounds(22, 208, 120, 28);
 		add(lblFirstName);
 		
 		JLabel lblLastName = new JLabel("Last name:");
 		lblLastName.setFont(new Font("Dialog", Font.PLAIN, 16));
-		lblLastName.setBounds(146, 317, 120, 28);
+		lblLastName.setBounds(334, 212, 120, 28);
 		add(lblLastName);
+		
+		JLabel lblPhone = new JLabel("Phone:");
+		lblPhone.setFont(new Font("Dialog", Font.PLAIN, 16));
+		lblPhone.setBounds(22, 250, 120, 28);
+		add(lblPhone);
+		
+		textField_phone = new JTextField();
+		textField_phone.setEditable(false);
+		textField_phone.setColumns(10);
+		textField_phone.setBounds(154, 250, 148, 28);
+		add(textField_phone);
+		
+		textField_email = new JTextField();
+		textField_email.setEditable(false);
+		textField_email.setColumns(10);
+		textField_email.setBounds(466, 250, 148, 28);
+		add(textField_email);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setFont(new Font("Dialog", Font.PLAIN, 16));
+		lblEmail.setBounds(334, 250, 120, 28);
+		add(lblEmail);
+		
+		textField_adress = new JTextField();
+		textField_adress.setEditable(false);
+		textField_adress.setColumns(10);
+		textField_adress.setBounds(154, 292, 148, 28);
+		add(textField_adress);
+		
+		JLabel lblAdress = new JLabel("Adress:");
+		lblAdress.setFont(new Font("Dialog", Font.PLAIN, 16));
+		lblAdress.setBounds(22, 292, 120, 28);
+		add(lblAdress);
 	}
 }

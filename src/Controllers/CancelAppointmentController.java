c6d09a37078f50ea2052de251c0a383b3e86cbf4
yshavat89 <yshavat89@gmail.com>
2016-06-06 12:21:25 +0300

    Revert "Revert "cancelAppointmentEntity General Manager view General Manager controller cancel appointment view cancel appointment controller""
    This reverts commit 1419aaf86f1eb5f14ee48c3f30b387a885502577.

--- src/Controllers/CancelAppointmentController.java
+++ src/Controllers/CancelAppointmentController.java
@@ -6,39 +6,60 @@ import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
+import mainPackage.MainClass;
 import ocsf.server.GHealthServer;
 import views.CancelAppointmentView;
 import Controllers.IRefresh;
+import Entities.Appointment;
+import Entities.CancelAppointmentEntity;
+import Entities.Patient;
 
 public class CancelAppointmentController implements Observer,IRefresh  {
 	public  CancelAppointmentView  CancelAppointmentview;
+	public CancelAppointmentEntity CAP;
 	
 	public CancelAppointmentController() {
 		 CancelAppointmentview = new  CancelAppointmentView();
 
 	}
+	public void searchAppointments(){
+		CAP = new CancelAppointmentEntity("search" , CancelAppointmentview.getSearchField().getText());
+		 MainClass.ghealth.sendMessegeToServer(CAP);
+	}
 	
-	public void toSQL(String idpatient )
+	public void searchAppintmentSQL(CancelAppointmentEntity cap )
 	{
 				ArrayList<Object> arrList = new ArrayList<Object>();
-				String query="	SELECT  ghealth.appointments.appdate,ghealth.expert.experties,ghealth.users.firstname,ghealth.users.lastname"
-								+"FROM ghealth.appointments,ghealth.users,ghealth.expert" 
-								+"WHERE ghealth.appointments.idpatient="+ idpatient+"AND"
-								+"ghealth.appointments.idexpert = ghealth.expert.id AND"
-								+"ghealth.users.username = ghealth.expert.id";
+				String query="SELECT ghealth.appointments.appdate,ghealth.expert.experties,ghealth.users.firstname,ghealth.users.lastname"
+								+" FROM ghealth.appointments,ghealth.users,ghealth.expert" 
+								+" WHERE ghealth.appointments.idpatient=" + cap.getIdPatient()+" AND"
+								+" ghealth.appointments.idexpert = ghealth.expert.id AND"
+								+" ghealth.users.username = ghealth.expert.id";
 				arrList = GHealthServer.sqlConn.sendSqlQuery(query);
+				System.out.println(arrList);
+				
+
 				for (int i  = 0 ; i < arrList.size() ; i +=4)
-				CancelAppointmentview.getComboBox().addItem((String)arrList.get(i)+" "+ (String)arrList.get(i+1)+" "+(String)arrList.get(i+2)+" "+(String)arrList.get(i+3));
+				{
+					cap.getAppList().add(new Appointment(arrList.get(i).toString(),(String)arrList.get(i+1),(String)arrList.get(i+2),(String)arrList.get(i+3)));
+				}
+				
 	}
 		
 	@Override
 	public void refreshView() {
-		// TODO Auto-generated method stub
-		
+		MainClass.masterControler.setView(MainClass.masterControler.CACont.CancelAppointmentview);		
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+		//if(arg)
+		/*if (arg instanceof Appointment )
+		{
+			
+			for (int i  = 0 ; i < appList.size() ; i ++)
+			CancelAppointmentview.getComboBox().addItem(appList.get(i).getAppDate()+" "+ appList.get(i).getExperties()+" "+appList.get(i).getFirstName()+" "+appList.get(i).getLastName());
+			refreshView();
+		}*/
 	}
 }

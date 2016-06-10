7431d11f3080fba31ea9d32e615fc34e05bc564f
Ruslan <Ruslan@10.0.0.151>
2016-06-10 12:21:10 +0300

    update

--- src/Controllers/RequestDetailsController.java
+++ src/Controllers/RequestDetailsController.java
@@ -3,15 +3,29 @@ package Controllers;
 import java.util.Observable;
 import java.util.Observer;
 
+import mainPackage.MainClass;
 import views.RequestDetailsView;
 import Controllers.IRefresh;
+import Entities.MedicalFile;
 
 public class RequestDetailsController implements Observer,IRefresh  {
 	public RequestDetailsView RequestDetailsview;
+	public MedicalFile mf;
 	
 	public RequestDetailsController() {
 		RequestDetailsview = new RequestDetailsView();
 	}
+	
+	public void getMedicalFile(String patID){
+		mf = new MedicalFile(patID);
+		MainClass.ghealth.sendMessegeToServer(mf);
+	}
+	
+	public void serverGetMedicalFile(MedicalFile MF){
+		
+	
+	}
+	
 	@Override
 	public void refreshView() {
 		// TODO Auto-generated method stub

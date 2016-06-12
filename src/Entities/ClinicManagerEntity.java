28358c1d828ea4f3dc4b834ba2d20fde2be8728d
yshavat89 <yshavat89@gmail.com>
2016-06-13 02:02:41 +0300

    fix

--- src/Entities/ClinicManagerEntity.java
+++ src/Entities/ClinicManagerEntity.java
@@ -1,8 +1,12 @@
 package Entities;
 
 import java.io.Serializable;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
+import ocsf.server.GHealthServer;
 
 public class ClinicManagerEntity implements Serializable{
 
@@ -14,8 +18,32 @@ public class ClinicManagerEntity implements Serializable{
 	private int ManagerId;
 	private Date from;
 	private Date to;
+	private ArrayList<AppointmentTimeValues> week = new ArrayList<AppointmentTimeValues>();
 	private ArrayList<AppointmentTimeValues> month = new ArrayList<AppointmentTimeValues>();
-	private Date day;
+	private ArrayList<AppointmentTimeValues> day = new ArrayList<AppointmentTimeValues>();
+	
+	public void createDayliReport(Date date)
+	{
+		ArrayList<Object> arrList = new ArrayList<Object>();
+		String query ="SELECT app.dispatcherSettingDate,app.dispatcherSettingHour,app.appdate,app.start,app.end,app.realStart,app.realEnd FROM ghealth.appointments as app where app.appdate="+generateDayDateToSql(date)+";";
+		arrList = GHealthServer.sqlConn.sendSqlQuery(query);
+		for(int i = 0 ; i < arrList.size() ; i+=7)
+			day.add(new AppointmentTimeValues((Date)arrList.get(i),(Time)arrList.get(i+1),(Date)arrList.get(i+2),(Time)arrList.get(i+3),(Time)arrList.get(i+4) ,(Time)arrList.get(i+5),(Time)arrList.get(i+6)));
+	}
+	
+	public void createWeeklyReport(Date date)
+	{
+		
+	}
+	public void createMonthlyReport(Date date)
+	{
+		ArrayList<Object> arrList = new ArrayList<Object>();
+		String query ="SELECT app.dispatcherSettingDate,app.dispatcherSettingHour,app.appdate,app.start,app.end,app.realStart,app.realEnd FROM ghealth.appointments as app where app.appdate="+generateMonthDateToSql(date)+";";
+		arrList = GHealthServer.sqlConn.sendSqlQuery(query);
+		for(int i = 0 ; i < arrList.size() ; i+=7)
+			month.add(new AppointmentTimeValues((Date)arrList.get(i),(Time)arrList.get(i+1),(Date)arrList.get(i+2),(Time)arrList.get(i+3),(Time)arrList.get(i+4) ,(Time)arrList.get(i+5),(Time)arrList.get(i+6)));
+	
+	}
 	
 	public String getTaskToDo() {
 		return taskToDo;
@@ -42,10 +70,18 @@ public class ClinicManagerEntity implements Serializable{
 		this.from = from;
 	}
 	public Date getTo() {
+		
 		return to;
 	}
 	public void setTo(Date to) {
 		this.to = to;
+	}
+	
+	public ArrayList<AppointmentTimeValues> getWeek() {
+		return week;
+	}
+	public void setWeek(ArrayList<AppointmentTimeValues> week) {
+		this.week = week;
 	}
 	public ArrayList<AppointmentTimeValues> getMonth() {
 		return month;
@@ -53,13 +89,30 @@ public class ClinicManagerEntity implements Serializable{
 	public void setMonth(ArrayList<AppointmentTimeValues> month) {
 		this.month = month;
 	}
-	public Date getDay() {
+	public ArrayList<AppointmentTimeValues> getDay() {
 		return day;
 	}
-	public void setDay(Date day) {
+	public void setDay(ArrayList<AppointmentTimeValues> day) {
 		this.day = day;
 	}
-                            
+	public String generateDayDateToSql(Date date)
+	{
+		
+		return new SimpleDateFormat("yyyyMMdd").format(this.to);
+	}
+	public String generateMonthDateToSql(Date date)
+	{
+		String Mdate= new SimpleDateFormat("yyyyMMdd").format(this.to);
+		return Mdate.substring(0,5)+"__";
+	}
+	public String getTo_Sql()
+	{
+		return new SimpleDateFormat("yyyyMMdd").format(this.to);	 //format for update mySql workbench
+	}
 
+	public String getFrom_Sql()
+	{
+		return new SimpleDateFormat("yyyyMMdd").format(this.from); 	
+	}
 	
 }
